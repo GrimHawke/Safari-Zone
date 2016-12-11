93d79d5c700d657608e8d0fcd101e2cd5f10263f
Evan Teng <Evan Teng>
2016-12-10 23:11:33 -0500

    Character movement on the map, state change based on clicks and keypresses.

--- public/scripts/testreact.js
+++ public/scripts/testreact.js
@@ -21,34 +21,89 @@ var Sidebar = React.createClass({
 });
 
 var Map = React.createClass({
+  keyPressed: function(event){
+    const x = this.props.getX();
+    const y = this.props.getY();
+    if(event.key == " "){
+      this.props.changeState();
+    }
+    else if(event.key == "w"){
+      if(y!=0){this.props.setY(y-1);}
+    }
+    else if(event.key == "s"){
+      if(y!=17){this.props.setY(y+1);}
+    }
+    else if(event.key == "a"){
+      if(x!=0){this.props.setX(x-1);}
+    }
+    else if(event.key == "d"){
+      if(x!=23){this.props.setX(x+1);}
+    }
+  },
+
   render: function() {
     var rows = [];
-    var height = 29;
-    var width = 37;
+    var height = 18;
+    var width = 24;
     for(var i = 0; i < height; i++) {
       var columns = [];
       for(var j=0; j < width; j++) {
         var classstring = "row" + i + " column" + j;
         var classcolor = "";
         if (i==9 || i > 9 && j == Math.round(height/2)+7) {
-          columns.push(<td className={classstring} style={{backgroundColor:'darkslategray'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'darkslategray'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'darkslategray'}}></td>);
+          }
         } else if(j==6|| j==7 || j>6 && (j+15-i) < 2 || i > height-3 && j > 4 && j < (Math.round(width/3)*2 + i - Math.round(width/2))) {
-          columns.push(<td className={classstring} style={{backgroundColor:'blue'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'blue'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'blue'}}></td>);
+          }
         } else if((i+4)%7 < 5 && (j+2)%5 < 3 && (i+j)%9 < 4) {
-          columns.push(<td className={classstring} style={{backgroundColor:'darkgreen'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'darkgreen'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'darkgreen'}}></td>);
+          }
         } else if((Math.round(i/2)*3+4)%12 > 8 && (j+2)%9 < 4 && (i*2+Math.round(j/3)*2+3)%9 < 7) {
-          columns.push(<td className={classstring} style={{backgroundColor:'royalblue'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'royalblue'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'royalblue'}}></td>);
+          }
         } else if((i*3-i^2+5)%10 < 7 && (j*2+3)%9 < 7 && (i*2+j)%10 < 7) {
-          columns.push(<td className={classstring} style={{backgroundColor:'green'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'green'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'green'}}></td>);
+          }
         } else if((i*4-i^2+4)%13 < 9 && (j*2+3)%10 < 8 && (i*2+j+5)%10 < 7) {
-          columns.push(<td className={classstring} style={{backgroundColor:'saddlebrown'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'saddlebrown'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'saddlebrown'}}></td>);
+          }
         } else {
-          columns.push(<td className={classstring} style={{backgroundColor:'sandybrown'}}></td>);
+          if(this.props.getY()==i && this.props.getX()==j) {
+            columns.push(<td className={classstring} style={{backgroundColor:'sandybrown'}}><img src="pictures/redow.png"/></td>);
+          }
+          else {
+            columns.push(<td className={classstring} style={{backgroundColor:'sandybrown'}}></td>);
+          }
         }
       }
       rows.push(<tr>{columns}</tr>);
     }
-    return (<table className="bird">{rows}</table>);
+    return (<table className="bird" tabIndex="0" onKeyDown={this.keyPressed}>{rows}</table>);
   }
 });
 
@@ -56,7 +111,8 @@ var BattleBoy = React.createClass({
   getInitialState: function() {
     return({
       pokemon: "missingno",
-      rarity: 0
+      rarity: 0,
+      caught: 0
     })
   },
 
@@ -75,13 +131,17 @@ var BattleBoy = React.createClass({
     return "pictures/" + this.state.pokemon + ".png";
   },
 
+  changeStateAndCatch: function(){
+    this.setState({caught: 1}, () => {this.props.changeState();});
+  },
+
   componentWillUnmount: function() {
-    this.props.updateScore(this.state.rarity);
+    this.props.updateScore(this.state.rarity*this.state.caught);
     this.props.updateTurns();
   },
 
   render: function() {
-    return(<table className="gaem" style={{backgroundImage:'url(pictures/backgroundboy.png)'}}>
+    return(<table className="game" style={{backgroundImage:'url(pictures/backgroundboy.png)'}}>
       <tr>
         <td>
         </td>
@@ -97,7 +157,7 @@ var BattleBoy = React.createClass({
         </td>
       </tr>
       <tr className="ui">
-        <td>
+        <td onClick={this.changeStateAndCatch}>
           BALL
         </td>
         <td>
@@ -108,7 +168,7 @@ var BattleBoy = React.createClass({
         <td>
           ROCK
         </td>
-        <td>
+        <td onClick={this.props.changeState}>
           RUN
         </td>
       </tr>
@@ -118,11 +178,34 @@ var BattleBoy = React.createClass({
 
 var GameScreen = React.createClass({
   getDefaultProps: function() {
-    return{"score": 0, "turns": 10}
+    return({
+      "score": 0,
+      "turns": 10,
+      "xloc": 10,
+      "yloc": 10
+    });
+  },
+
+  setX: function(newX) {
+    this.props.xloc = newX;
+    this.forceUpdate();
+  },
+
+  getX: function() {
+    return this.props.xloc;
+  },
+
+  setY: function(newY){
+    this.props.yloc = newY;
+    this.forceUpdate();
+  },
+
+  getY: function() {
+    return this.props.yloc;
   },
 
   getInitialState: function(){
-    return{gameState: 0}
+    return{gameState: 1}
   },
 
   changeState: function(){
@@ -150,15 +233,15 @@ var GameScreen = React.createClass({
     var message = "";
     if (this.state.gameState == 0){
       message = (
-        <div className="col-xs-9 game-item" onClick={this.changeState}>
-          <BattleBoy updateScore={this.updateScore} updateTurns={this.updateTurns}/>
+        <div className="col-xs-9 game-item">
+          <BattleBoy updateScore={this.updateScore} updateTurns={this.updateTurns} changeState={this.changeState} />
         </div>
       )
     }
     else if (this.state.gameState == 1){
       message = (
-        <div className="col-xs-9 game-item" onClick={this.changeState}>
-          <Map />
+        <div className="col-xs-9 game-item">
+          <Map changeState={this.changeState} getX={this.getX} getY={this.getY} setX={this.setX} setY={this.setY}/>
         </div>
       )
     }
